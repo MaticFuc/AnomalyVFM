a34fd8477bf9ae9e87d9cf391e4d8531ddca4e89
Matic Fučka <maticfuc@gmail.com>
2026-04-20 11:04:10 +0200

    forgot to update radio

--- models/radio.py
+++ models/radio.py
@@ -4,17 +4,24 @@ import torchvision.transforms as T
 
 from peft_local.peft_func import add_peft
 
+from radio_local import RADIOModel
+
 
 class RADIO(nn.Module):
 
-    def __init__(self, height):
+    def __init__(self, height, use_local = False):
         super().__init__()
-        self.net = torch.hub.load(
-            "NVlabs/RADIO", "radio_model", version="radio_v2.5-l", skip_validation=True
-        )
+        if use_local:
+            self.net = RADIOModel()
+        else:
+            self.net = torch.hub.load(
+                "NVlabs/RADIO", "radio_model", version="radio_v2.5-l", skip_validation=True
+            )
+        
         self.feature_dim = 1024
         self.patch_size = 16
         self.H = height
+        self.local_impl = use_local
 
     def get_img_transform(self):
         return T.Compose(
@@ -29,4 +36,7 @@ class RADIO(nn.Module):
 
     def forward(self, x):
         output = self.net(x)
+
+        if self.local_impl:
+            return output[0], output[1]
         return output.summary, output.features
